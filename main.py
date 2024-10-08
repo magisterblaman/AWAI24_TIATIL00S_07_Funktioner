32a3e4c084b50f74c6cc27eadb67580dae19a1ab
magisterblaman <hannes.blaman@lbs.se>
2024-10-08 10:11:43 +0200

    Retur

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def my_for_loop(times, message):
-    for i in range(times):
-        print(message + str(i))
+def multiply(a, b):
+    prod = a * b
+    return prod
 
-my_for_loop(4, "Hej")
-my_for_loop(10, "Då")
-my_for_loop(51, "Re")
-my_for_loop(2, "Mi")
-my_for_loop(0, "Fa")
+product = multiply(23, 3)
+
+print(product)
