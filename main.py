ca507c29c4d1fecc64da176f82c457faabb195ae
magisterblaman <hannes.blaman@lbs.se>
2024-10-08 09:52:27 +0200

    Funktioner och parametrar

---
+++ main.py
@@ -0,0 +1,9 @@
+def my_for_loop(times, message):
+    for i in range(times):
+        print(message + str(i))
+
+my_for_loop(4, "Hej")
+my_for_loop(10, "Då")
+my_for_loop(51, "Re")
+my_for_loop(2, "Mi")
+my_for_loop(0, "Fa")
